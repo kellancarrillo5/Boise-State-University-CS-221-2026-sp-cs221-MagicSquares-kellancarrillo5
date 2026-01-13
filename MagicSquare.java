afcb29a8483154a8ccdb719855a7888b88ec267c
Kellan Carrillo <kellancarrillo@u.boisestate.edu>
2026-01-13 16:41:26 -0700

    attempt

--- MagicSquare.java
+++ MagicSquare.java
@@ -56,6 +56,7 @@ public class MagicSquare implements MagicSquareInterface{
 
          throw new UnsupportedOperationException("Unimplemented method 'toString'");
     }
+    
 
 }
 
--- MagicSquareDriver.java
+++ MagicSquareDriver.java
@@ -1,17 +1,21 @@
 /**
  * Driver class for Magic Square
+ * 
  * @author kellancarrillo5
  */
 
 public class MagicSquareDriver {
-    public static void main(String[] args) {
-        //instance variables
-        private String selectedOption;
-        private int givenSize;
-        private String fileName;
+    // instance variables
+    private String selectedOption;
+    private int givenSize;
+    private String fileName;
+    private int[][] magicSquare;
+    private int row;
+    private int col;
 
+    public static void main(String[] args) {
          //display to the command line the selections avaliable, -check or -create
-        //System.out.println("java MagicSquareDriver <-check | -create> <filename> < |size>");
+        System.out.println("java MagicSquareDriver <-check | -create> <filename> < |size>");
          
         //if command line == 2 arguements (-check)
         if(args.length == 2){
@@ -19,52 +23,48 @@ public class MagicSquareDriver {
         }
         
         //if command line has 3 arguments (-create)
+
+        /**
+        * Constructor to create a magic square
+        * @param fileName : input fileName 
+        * @param givenSize : set col/row size of the 2D magicSquare
+        */
+        public MagicSquare(String fileName, int givenSize){
+            //initialize instance variables 
+            this.givenSize = parseInt(args[3]);
+            magicSquare = new int[givenSize][givenSize];
+            this.row = givenSize - 1; 
+            this.col = givenSize / 2;
+
+            
+            this.fileName = args[1];
+            this.selectedOption = args[0];
+            
+            //check that size is an odd positive integer (size is not the magic number its length for 2D array)
+            if(givenSize / 2 == 0 || givenSize < 0){
+                System.out.println("Selected size must be an odd positive integer, try again.");
+            } else {
+            //printwriter?
+            }
+        }
+
+        /**
+        * Constructor to check if a file is a valid magic square
+        * @param fileName : input fileName
+        */
+        public MagicSquare(fileName){
+            //update instance varibales
+            //
+        }
+
         return;
         }
-    }
-   
-    //new int file made
-
-
-    //scanner for command line, look for -check and -create
-
-    //Method to check if the 
-
-    /**
-     * Constructor to create a magic square
-     * @param fileName : input fileName 
-     * @param givenSize : set col/row size of the 2D magicSquare
-     */
-    public MagicSquare(fileName, givenSize){
-        //update instance variables 
-        this.fileName = args[1];
-        this.selectedOption = args[0];
-        this.givenSize = parseInt(args[3]);
-
-        //check that size is an odd positive integer (size is not the magic number its length for 2D array)
-        if(givenSize / 2 == 0 || givenSize < 0){
-            System.out.println("Selected size must be an odd positive integer, try again.");
-        } else {
-
-        }
-
-        }
-        //printwriter?
-
 
     }
 
-    /**
-     * Constructor to check if a file is a valid magic square
-     * @param fileName : input fileName
-     */
-    public MagicSquare(fileName){
-        //update instance varibales
 
-        //
-    }
+    // new int file made
 
-   
-    
-}
+    // scanner for command line, look for -check and -create
 
+    // Method to check if the
